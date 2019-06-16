65be05a3f17ab8dcdeb4da82788ccbd9d3ade280
xiaoming <xiaoming@sina.com>
2019-06-16 22:31:09 +0800

    第二次提交代码

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
+// 1.1导入路由的包
+import VueRouter from 'vue-router'
+// 1.2安装路由
+Vue.use(VueRouter)
 
-import app from './App.vue'
+
 
 // 导入Mint-UI组件
 import {Header} from 'mint-ui'
@@ -9,8 +13,17 @@ Vue.component(Header.name, Header)
 
 // 导入MUI样式
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+
+
+// 1.3导入自己的router.js 路由模块
+import router from './router.js'
+
+
+import app from './App.vue'
 
 const vm = new Vue({
   el: '#app',
-  render: c => c(app)
+  render: c => c(app),
+  router // 1.4挂载路由对象到 vm 实例上
 })
